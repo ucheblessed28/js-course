a985d481fedd04a0179bb905a8664a369b4ffbd4
Uche Blessed <ucheblessed1@gmail.com>
2025-01-28 14:08:22 +0100

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,3 +1,36 @@
-let someone = {
+let michael = {
     firstname: 'Michael',
+    lastName: 'John',
+    age: 13,
+    favColor: 'Red',
+    favFood: 'Bread'
 }
+
+let alice = {
+    firstname: 'Alice',
+    lastName: 'John',
+    age: 11,
+    favColor: 'Blue',
+    favFood: 'Grapes'
+}
+
+console.log(michael.favColor, alice.favColor,);
+
+let sandra = {
+    firstname: 'Sandra',
+    lastname: 'Ben',
+    color: 'blue',
+    stack: 'frontend',
+}
+
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    greet: function () {
+      return `Hello, my name is ${this.firstName} ${this.lastName}`;
+    },
+  };
+  
+  console.log(person.greet());  // Output: Hello, my name is John Doe`
+
